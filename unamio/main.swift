fb21da5379549d01e69b73c6ff55e39246f6f06a
Julio César Guzmán <julio.guzman@me.com>
2016-06-04 13:08:23 -0500

    Initial Commit

---
+++ unamio/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  unamio
+//
+//  Created by Julio César Guzman on 6/4/16.
+//  Copyright © 2016 Julio César Guzman. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
